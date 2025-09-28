7f45ad42d29e46e3c4dffa9fcaaebe34bf25ad7a
Shritej Koneru <mail4shritejkoneru@gmail.com>
2025-09-28 16:43:29 +0530

    Delete backend directory(OLD)

--- backend/utils/zip_utils.py
+++
@@ -1,7 +0,0 @@
-import zipfile
-
-def zip_files(filepaths, zip_path, arc_names=None):
-    with zipfile.ZipFile(zip_path, 'w') as zf:
-        for i, f in enumerate(filepaths):
-            arc_name = arc_names[i] if arc_names else None
-            zf.write(f, arcname=arc_name or f.split("/")[-1])
